f567f20e6469878042273a61d6517c5476e918ff
angelDova <Angelfcb01@yahoo.com>
2024-01-01 20:40:11 -0800

    2: typings

--- components/genre-dropdown.tsx
+++ components/genre-dropdown.tsx
@@ -1,6 +1,23 @@
+import { Genres } from "@/typings";
 import React from "react";
 
-function GenreDropdown() {
+async function GenreDropdown() {
+  const url = "https://api.themoviedb.org/3/genre/list?language=en";
+
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+    next: {
+      revalidate: 60 * 60 * 24, // 24 hours
+    },
+  };
+
+  const response = await fetch(url, options);
+  const data = (await response.json()) as Genres;
+
   return <div>GenreDropdown</div>;
 }
 
---
+++ typings.ts
@@ -0,0 +1,3 @@
+export type Genre = { id: number; name: string };
+
+export type Genres = { genres: Genre[] };
